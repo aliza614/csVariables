317670410bf9c47cd1488e45f9dd5c63e932eb3b
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-05-25 22:32:06 -0600

    committing my program

---
+++ csVariablesProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csVariablesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Lucie";
+            int age = 8;
+            char lastInital = 'O';
+            bool isYoung = true;
+            double heightFt = 4.5;
+            decimal weight = 100.3m;
+
+            Console.WriteLine($"I love {name} {lastInital}.  She is {age} years old.  It is {isYoung} that she is young.  She is {heightFt} feet tall and weighs {weight} lbs");
+        }
+    }
+}
